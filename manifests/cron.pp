4528e85f014d190f794416caf704088ac4baef92
RedPaes <pascal@meyer-cloud.ch>
2019-12-21 23:12:58 +0100

    Add cron for occ filescan

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -1,9 +1,19 @@
 class nextcloud::cron () {
-  cron { 'nextcloud':
+  cron { 'nextcloud-cronjob':
     ensure  => 'present',
     command => "php -f ${nextcloud::install::install_dir}/cron.php",
     minute  => ['*/5'],
     target  => 'www-data',
     user    => ' www-data',
   }
+
+  cron { 'nextcloud-scan-files':
+    ensure  => 'present',
+    command => "sudo -u www-data php nextcloud/occ files:scan --all",
+    minute  => ['15'],
+    hour    => ['*/2'],
+    target  => 'www-data',
+    user    => ' www-data',
+  }
+
 }
